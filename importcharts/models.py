373dab4abc57cfdcb90de2767d1a335b2a67bd97
voltsrage <electric5388@hotmail.com>
2023-03-23 17:57:07 +0800

    Add import charts excel functionality and paging table

--- importcharts/models.py
+++ importcharts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from quotation.models import Country,Animal
 
 # Create your models here.
+class Import(models.Model):
+	commodity_code = models.CharField(max_length=13)
+	month = models.DateField(blank=True, null=True)
+	animal = models.ForeignKey(Animal,on_delete=models.SET_NULL, blank=True, null=True)
+	production_description = models.CharField(max_length=250)
+	country = models.ForeignKey(Country,on_delete=models.SET_NULL, blank=True, null=True)
+	country_name = models.CharField(max_length=50)
+	total_price = models.DecimalField(max_digits=12,decimal_places=2)
+	total_weight_tons = models.DecimalField(max_digits=7,decimal_places=2)
+	total_weight_kg = models.DecimalField(max_digits=9,decimal_places=2)
+	price_per_kg = models.DecimalField(max_digits=5,decimal_places=2)
+
+	def __str__(self):
+		return f'{self.country}-{self.production_description}-{self.total_price}'
+
+	class Meta:
+		unique_together  = ["commodity_code","month",'country','production_description']
--- importcharts/views.py
+++ importcharts/views.py
@@ -1,16 +1,32 @@
 import pandas as pd
+import datetime
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
+from django.views.decorators.http import require_GET
+from django.core.paginator import Paginator
+from .models import Import
+from quotation.models import Country
 
 # Create your views here.
 def ImportChartListViewSet(request):
+	imports = Import.objects.all()
 
+	pageSize = request.GET.get('pageSize')
+
+	if pageSize is None:
+		pageSize = 10
+
+	paginator = Paginator(imports,pageSize)
+	page = request.GET.get('page')
+	data = paginator.get_page(page)
 	context = {
-	}
+				"data":data
+			}
 	return render(request,'importcharts/list.html',context)
 
 
 def import_file(request):
+	data = Import.objects.all()
 	if request.method == 'POST' and request.FILES['file']:
 			file = request.FILES['file']
 			if file.name.endswith('.xlsx'):
@@ -23,25 +39,80 @@ def import_file(request):
 			my_list = df.values.tolist()
 
 			for i,item in enumerate(my_list):
-				if 'shrimp' in item[3] | 'Shrimp' in item[3]:
-					print('has_shrimp')
-				elif 'salmon' in item[3] | 'Salmon' in item[3]:
-					print('has_salmon')
-				elif 'lobster' in item[3] | 'Lobster' in item[3]:
-					print('has_lobster')
-				elif 'Scallops' in item[3] | 'scallops' in item[3]:
-					print('has_scallops')
+				import_item = Import(
+					commodity_code=item[2],
+					production_description=item[3],
+					total_price=item[5]*1000,
+					total_weight_tons=item[6],
+					total_weight_kg=item[7]
+					)
+				if 'shrimp' in item[3] or 'Shrimp' in item[3]:
+					import_item.animal_id = 1
+				elif 'salmon' in item[3] or 'Salmon' in item[3]:
+					import_item.animal_id = 2
+				elif 'lobster' in item[3] or 'Lobster' in item[3]:
+					import_item.animal_id = 3
+				elif 'Scallops' in item[3] or 'scallops' in item[3]:
+					import_item.animal_id = 4
 
 				if "Viet Nam" in item[4]:
-					item[4] = 'VietName'
+					item[4] = 'Vietnam'
+
+				if "Korea, Republic of" in item[4]:
+					item[4] = 'Korea (South)'
+
+				if "Russian Federation" in item[4]:
+					item[4] = 'Russia'
+
+				if "Islamic Republic of Iran" in item[4]:
+					item[4] = 'Iran'
+
+				if "Arab Emirates, United" in item[4]:
+					item[4] = 'United Arab Emirates'
+
+				if "Taiwan, ROC" in item[4]:
+					item[4] = 'Taiwan'
+
+				if "Federated States of Micronesia" in item[4]:
+					item[4] = 'Micronesia'
+
+				if "Korea, Democratic People's Republic of" in item[4]:
+					item[4] = 'Korea (North)'
+
+				if "Moldova, Republic of" in item[4]:
+					item[4] = 'Moldova'
+
+				import_item.country_name = item[4]
+
+				country = Country.objects.get(name=item[4])
+
+				import_item.country = country
 
 				if(item[7] != 0):
 					price_per_kg = item[5]*1000/item[7]
 				else:
 					price_per_kg = 0
 
-				print(f'{item[0]},{item[3]}, {item[4]}, {round(price_per_kg,3)}')
+				import_item.price_per_kg = price_per_kg
 
-			return JsonResponse({'success': True, 'list':my_list})
+				year = item[1].split('/')[0]
+				month = item[1].split('/')[1]
+				if int(month) < 10:
+					month = f'0{month}'
+
+				date = f'{year}-{month}-01'
+				month_for_db = datetime.datetime.strptime(date, '%Y-%m-%d').date()
+
+				import_item.month = month_for_db
+
+				import_item.save()
+				data = Import.objects.all()
+				paginator = Paginator(data,10)
+				page = request.GET.get('page')
+				data = paginator.get_page(page)
+				context = {
+					"data":data
+				}
+			return render(request, 'importcharts/list.html',context)
 
 	return render(request, 'importcharts/list.html')
